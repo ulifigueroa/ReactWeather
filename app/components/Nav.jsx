75c204ac061be5713de6a3e39d38962b1674b44b
Ulises Figueroa <ulifigueroa@gmail.com>
2017-09-01 18:01:46 -0500

    Adding style to the top nav bar.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,34 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-    return (
-        <div>
-            <h2>Nav Component</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        </div>
-    );
-};
+var Nav = React.createClass({
+    onSearch: function(event) {
+        alert('Not yet implemented');
+        event.preventDefault();
+    },
+
+    render: function() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+                        <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+                        <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Enter a city name"/></li>
+                            <li><button type="submit" className="button">Get Weather</button></li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
 
 module.exports = Nav;
