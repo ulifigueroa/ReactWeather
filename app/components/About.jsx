6325ee4f406bed88b699c9f7e49af1f36cca7b87
Ulises Figueroa <ulifigueroa@gmail.com>
2017-09-01 18:19:15 -0500

    Adding details to the about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,15 @@ var React = require('react');
 
 var About = () => {
     return (
-        <h1 className="text-center">About Component</h1>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application build on React.</p>
+            <p>Here are some of the tools I used:</p>
+            <ul>
+                <li><a href="https://facebook.github.io/react" target="_blank">React</a></li>
+                <li><a href="https://openweathermap.org" target="_blank">Open Weather Map</a></li>
+            </ul>
+        </div>
     );
 };
 
